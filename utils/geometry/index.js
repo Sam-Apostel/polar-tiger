72a9f1e3999005b47eb98aca7f8744b3c7411e9a
sam@apostel.be <sam@apostel.be>
2022-03-31 22:56:23 +0200

    v 0.7.1 util fix fix segments for custom cylinder

--- utils/geometry/index.js
+++ utils/geometry/index.js
@@ -1,9 +1,9 @@
 const jscad = require('@jscad/modeling');
 const { cylinderElliptic } = jscad.primitives;
 
-const myCylinder = (height, start, end, segments = 32) => {
+const myCylinder = (height, start, end, segments = 64) => {
 	if (end === undefined) end = start;
-	return cylinderElliptic({ height, startRadius: [start, start], endRadius: [end, end] }, segments)
+	return cylinderElliptic({ height, startRadius: [start, start], endRadius: [end, end], segments })
 }
 
 module.exports = {
